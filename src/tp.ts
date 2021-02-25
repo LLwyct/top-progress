8e3b2ab3819bcdfbe2ba0939fe07216a84422ff9
LLwyct <787552171@qq.com>
2021-02-25 16:08:51 +0800

    使用RAF进行替换

--- src/tp.ts
+++ src/tp.ts
@@ -5,7 +5,7 @@ interface Options {
     color?: string
 };
 
-let defaultoptions: Options = {
+let defaultOptions: Options = {
     mounted: 'body',
     speed: '1s',
     width: '2px',
@@ -13,87 +13,147 @@ let defaultoptions: Options = {
 };
 
 class TP {
+  private main: HTMLElement;
+  private state: string;
+  private lengthpercent: number;
+  private settings: Options = defaultOptions;
 
-    private main: HTMLElement;
-    private state: string;
-    private lengthpercent: number;
-    private settings: Options = defaultoptions;
+  constructor(inputoptions: Options) {
+    this.settings = { ...this.settings, ...inputoptions };
+    this.main = this.mount();
+    this.init();
+  }
 
-    constructor (inputoptions: Options) {
-        this.settings = {...this.settings, ...inputoptions};
-        this.main = this.mount();
-        this.init();
+  mount(): HTMLElement {
+    /**
+     * 把tp挂载到body上
+     */
+    let bodyelemet = document.querySelector("body");
+    let topprogress = document.createElement("section");
+    topprogress.style.height = this.settings.width;
+    topprogress.style.transform = `translateX(-100%)`;
+    topprogress.style.backgroundColor = this.settings.color;
+    topprogress.setAttribute("id", "topprogress");
+    topprogress.innerHTML = `
+                <div class="bar">
+                    <div class="barshadow"></div>
+                </div>
+            `;
+    /**
+     * 后续可以作为异常抛出
+     */
+    if (bodyelemet) {
+      bodyelemet.append(topprogress);
     }
-    
-    mount (): HTMLElement {
-        /**
-         * 把tp挂载到body上
-         */
-        let bodyelemet = document.querySelector('body');
-        let topprogress = document.createElement('section');
-        topprogress.style.height = this.settings.width;
-        topprogress.style.transform = `translateX(-100%)`;
-        topprogress.style.backgroundColor = this.settings.color;
-        topprogress.setAttribute('id', 'topprogress');
-        topprogress.innerHTML = `
-        <div class="bar">
-        <div class="barshadow"></div>
-        </div>
-        `;
-        /**
-         * 后续可以作为异常抛出
-         */
-        if (bodyelemet) {
-            bodyelemet.appendChild(topprogress);
-        }
-        return topprogress;
-    }
-    
-    init(): void {
-        this.state = 'stop';
-        this.lengthpercent = 0;
-        this.main.style.transition = `transform ${this.settings.speed}s ease`;
-    }
+    return topprogress;
+  }
 
-    changeto(pos: number): void {
-        /**
-         * 可以考虑写成观察者，在修改lengthpercent的同时，同步修改tp的translate属性
-         */
-        if (pos>=0 && pos<=100) {
-            this.lengthpercent = pos;
-            this.main.style.transform = `translateX(-${100-pos}%)`;
-        }
-        return;
-    }
+  init(): void {
+    this.state = "stop";
+    this.lengthpercent = 0;
+  }
 
-    start(): void {
-        this.state = 'run';
-        let that = this;
-        function run(): void {
-            if (that.state === 'run') {
-                if (0 <= that.lengthpercent && that.lengthpercent <= 60) {
-                    that.changeto(that.lengthpercent + 10);
-                } else if (that.lengthpercent <= 75) {
-                    that.changeto(that.lengthpercent + 5);
-                } else if (that.lengthpercent <= 90) {
-                    that.changeto(that.lengthpercent + 3);
-                } else if (that.lengthpercent <= 95) {
-                    that.changeto(that.lengthpercent + 1);
-                } else {
-                    return;
-                }
-                run();
+  changeTo(pos: number): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      let that = this;
+      let lp = this.lengthpercent;
+      if (pos >= 0 && pos <= 100) {
+        let res = await this.animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            if (that.lengthpercent < 100) {
+              that.main.style.transform = `translateX(-${
+                100 - (lp + (pos - lp) * progress)
+              }%)`;
             }
+          },
+        });
+        this.lengthpercent = pos;
+        if (res === false) {
+          resolve(false);
+        } else {
+          resolve(true);
         }
-        requestAnimationFrame(run);
+      }
+    });
+  }
+  async goto(pos) {
+    this.state = "init";
+    await this.changeTo(pos);
+    this.state = "stop";
+  }
+  async start() {
+    this.state = "run";
+    while (this.state == "run") {
+      let res = true;
+      if (0 <= this.lengthpercent && this.lengthpercent <= 40) {
+        res = await this.changeTo(this.lengthpercent + 10);
+      } else if (this.lengthpercent <= 75) {
+        res = await this.changeTo(this.lengthpercent + 6);
+      } else if (this.lengthpercent <= 90) {
+        res = await this.changeTo(this.lengthpercent + 3);
+      } else if (this.lengthpercent <= 95) {
+        res = await this.changeTo(this.lengthpercent + 1.5);
+      } else {
+        break;
+      }
+      if (res === false) {
+        break;
+      }
     }
-    
-    done(): void {
-        this.main.style.transition = `transform 0.5s ease`;
-        this.changeto(100);
-        setTimeout(() => {
-            this.lengthpercent = 0;
-            this.changeto(0);
-        }, Number(this.settings.speed) * 1000);
-    }
+  }
+
+  async done() {
+    this.state = "init";
+    let lp = this.lengthpercent;
+    this.lengthpercent = 100;
+    let that = this;
+    await this.animate({
+      duration: 500,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        that.main.style.transform = `translateX(-${
+          100 - (lp + (100 - lp) * progress)
+        }%)`;
+      },
+    });
+    this.lengthpercent = 0;
+    await this.animate({
+      duration: 500,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        that.main.style.transform = `translateX(-${100 * progress}%)`;
+      },
+    });
+    this.state = "stop";
+  }
+
+  private animate({ timing, draw, duration }) {
+    let _this = this;
+    return new Promise((resolve) => {
+      let start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        if (_this.state == "run" || _this.state == "init") {
+          let timeFraction = (time - start) / duration;
+          if (timeFraction > 1) timeFraction = 1;
+          let progress = timing(timeFraction);
+          draw(progress);
+          if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+          } else {
+            resolve(true);
+          }
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  }
 }
